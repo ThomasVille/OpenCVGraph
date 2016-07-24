836af3050f32bf53018d5fda1a026abe81b3c264
WhiteMoll <c.thomas.quentin974@gmail.com>
2016-07-24 15:29:00 +0200

    Additional checks when connecting two nodes (different parents and free input)

--- OpenCVGraph/src/Parameter.cpp
+++ OpenCVGraph/src/Parameter.cpp
@@ -17,7 +17,20 @@ Parameter::~Parameter()
 
 bool Parameter::IsCompatible(std::shared_ptr<Parameter> other)
 {
-	return ( GetParamType() != other->GetParamType() ) && ( GetType().CompatibleWith(other->GetType()) );
+	// Check if the parent node is different
+	if (m_parent == other->GetParent())
+		return false;
+	// Check if the input is free
+	if ((m_paramType == INPUT_PARAM && !m_links.empty()) || (other->GetParamType() == INPUT_PARAM && !other->GetLinkedNodes().empty()))
+		return false;
+	// Check if we are linking an input with an output
+	if (GetParamType() == other->GetParamType())
+		return false;
+	// Check if the types are compatible
+	if (!GetType().CompatibleWith(other->GetType()))
+		return false;
+
+	return true;
 }
 
 void Parameter::AddLink(std::shared_ptr<Parameter> dstParam)
